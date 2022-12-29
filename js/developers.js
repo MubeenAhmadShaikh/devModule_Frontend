009588d691d76cdb33503b5a0a2c7dd4ce79afe7
MubeenAhmadShaikh <mubeen.shaikh7@yahoo.com>
2022-12-29 20:30:48 +0530

    Base url changes Test

--- js/developers.js
+++ js/developers.js
@@ -293,4 +293,150 @@ async function searchDevelopers(query,token){
 };
 
 
- 
+ // -------------- Function to show the account details for logged in user ------------
+function showAccountDetails(data){
+  let profile = data['profile'];
+  let projects = profile.project
+  // console.log(!profile.skill.length)
+  // let skills = profile.skill
+  let pfp_image;
+  if (profile.profile_image){
+    pfp_image = profile.profile_image
+  }else{
+    pfp_image = './images/default-profile.png'
+  }
+  if(profile.location){
+    profilelocation = `Based in ${profile.location}`
+  }else{
+    profilelocation =  ''
+  }
+  if(profile.short_intro){
+    intro = profile.short_intro;
+  }else{
+    intro = '<p>Developer</p>'
+  }
+
+  // Developer details left container column
+  let devInfoContainer = document.querySelector('.dev')
+  let devinfo= ""
+  devinfo += ` <div class="card__body dev">
+  <a class="tag tag--pill tag--main settings__btn" href="update-profile.html?id=${profile.id}"><i class="bi bi-pencil-square"></i> Edit</a>
+  <img class="avatar avatar--xl dev__avatar" src="${pfp_image}" />
+  <h2 class="dev__name">${profile.first_name} ${profile.last_name}</h2>
+  <p class="dev__title">${intro}</p>
+  <p class="dev__location">${profilelocation}</p>
+  <ul class="dev__social">
+    <li>
+    <a title="Github" href="#" target="_blank">
+    <span>
+    <i class="bi bi-github"></i>
+    </span>
+    </a>
+    </li>
+    <li>
+    <a title="LinkedIn" href="#" target="_blank">
+    <span>
+    <i class="bi bi-linkedin"></i>
+    </span>
+    </a>
+    </li>
+    <li>
+    <a title="Twitter" href="#" target="_blank">
+    <span>
+    <i class="bi bi-twitter"></i>
+    </span>
+    </a>
+    </li>
+    <li>
+    <a title="YouTube" href="#" target="_blank">
+    <span>
+    <i class="bi bi-youtube"></i>
+    </span>
+    </a>
+    </li>
+    <li>
+    <a title="Website" href="#" target="_blank">
+    <span>
+    <i class="bi bi-globe"></i>
+    </span>
+    </a>
+    </li>
+    
+  </ul>
+  <a href='javascript:;' onclick='deleteProfile();' class="btn btn--del btn--md"><i class="bi bi-x-circle"></i>&nbsp Deactivate Account</a>
+</div>
+`;
+devInfoContainer.innerHTML = devinfo;
+// Dev Info/AboutMe right container
+let aboutDevContainer = document.querySelector('.aboutdev')
+let aboutdev= ""
+if (!profile.bio){
+  aboutdev += `<h3 class='devInfo__title'>About Me</h3>
+  <h6 class='devSkill__title'>No description</h6>`
+  aboutDevContainer.innerHTML = aboutdev;
+}else{
+aboutdev += `<div class="settings">
+<h3 class="settings__title">About Me</h3>
+<a class="tag tag--pill tag--sub settings__btn tag--lg" href="update-profile.html?id=${profile.id}"><i class="bi bi-pencil-square"></i>&nbsp;Update</a>
+</div><br/>
+<p class="devInfo__about">
+  ${profile.bio}
+</p>`
+aboutDevContainer.innerHTML = aboutdev;
+}
+
+// Skills section
+let majorSkillsContainer = document.querySelector('.devmajorskills')
+// let extraSkillsContainer = document.querySelector('.devextraskills')
+let majorskills= ""
+if(!profile.skill.length){
+  majorSkillsContainer.innerHTML = "<h6 class='devSkill__title'>No skills mentioned</h6><br>";
+  // extraSkillsContainer.innerHTML = "<h6 class='devSkill__title'>No skills mentioned</h6>";
+}else{
+  console.log(data['profile']['skill'])
+  data['profile']['skill'].forEach((skill,i)=>{
+    majorskills += `<a href="update-skill.html?id=${skill.id}">
+    <span class="tag tag--pill tag--sub tag--lg">
+    <small>${skill.name}</small>&nbsp;
+    
+    <i class="bi bi-pencil-square"></i>
+    </span>
+    </a>
+    `
+    });
+    majorSkillsContainer.innerHTML = majorskills;
+
+};
+// Projects Section
+let devProjectsContainer = document.querySelector('.devprojects')
+let description = ''
+
+let devprojects= ""
+if (!projects.length){
+  devprojects += `
+  <h6 class='devSkill__title'>You have not uploaded any projects yet</h6>`
+}else{
+  projects.forEach((project,i)=>{
+    description=limit(project.description, 180);
+    devprojects += `
+    <tr>
+              <td class="settings__thumbnail">
+                <a href="single-project.html?id=${project.id}"><img src="${project.featured_image}" alt="Project Thumbnail" /></a>
+              </td>
+              <td class="settings__tableInfo">
+                <a href="single-project.html?id=${project.id}">${project.title}</a>
+                <p>
+                  ${description}
+                </p>
+              </td>
+              <td class="settings__tableActions">
+                <a class="tag tag--pill tag--main settings__btn" href="update-project.html?id=${project.id}"><i class="bi bi-pencil-square"></i>&nbsp Edit</a>
+                <a class="tag tag--pill tag--main settings__btn" href='javascript:;' onclick='deleteProject(${project.id});'><i class="bi bi-trash"></i>&nbspDelete</a>
+              </td>
+            </tr>
+    `
+  });
+};
+devProjectsContainer.innerHTML = devprojects;
+
+};
